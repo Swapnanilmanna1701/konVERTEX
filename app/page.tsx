058ce04459c5a156656410d9fc9a106571a082b8
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2025-01-30 12:28:32 +0530

    page fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,21 @@ import DropZone from "../components/dropzone";
 //import Image from "next/image";
 //import Link from "next/link";
 // import Image from "next/image";
+import GradientText from '../components/gradienttext'
+
 
 const Home = () => {
     return ( 
         <div className="space-y-12 pb-8 space-x-72 ">
         <div className="space-y-6">
-        <h1 className="text-center text-lime-400 font-serif text-6xl md:text-6xl gap-10 ">FileMagnet🚀</h1>
+        <h1 className="text-center text-bold  text-7xl md:text-6xl gap-10 "><GradientText
+  colors={["#0069ff", "#c148ff", "#ff17cb", "#17ecff", "#f4f4f4"]}
+  animationSpeed={3}
+  showBorder={false}
+  className="custom-class text-bold"
+>
+  FileFlex
+</GradientText></h1>
         <h2 className="text-center font-medium text-3xl md:text-5xl">
         Convert files with ease..
         <div className="relative inset-y-4">
